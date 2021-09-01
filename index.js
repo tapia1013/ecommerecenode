b43bf5f6ce54bd79b85d152e297988c07724b549
tapia1013 <manbearpig1013@gmail.com>
2021-08-31 21:20:05 -0700

    created form

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hi there');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <div>
+      <form>
+        <input placeholder="email" />
+        <input placeholder="password" />
+        <input placeholder="password confirmation" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
+});
+
+app.listen(3000, () => {
+  console.log('Server Running...');
+})
+
+
+
+
+
+
+
