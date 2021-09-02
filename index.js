3b68e9a5821c43b9120e7ad5c64d94b45ff602a7
tapia1013 <manbearpig1013@gmail.com>
2021-09-01 17:48:02 -0700

    forms method post and app.post to recieve response

--- index.js
+++ index.js
@@ -4,15 +4,20 @@ const app = express();
 app.get('/', (req, res) => {
   res.send(`
     <div>
-      <form>
-        <input placeholder="email" />
-        <input placeholder="password" />
-        <input placeholder="password confirmation" />
+      <form method="POST">
+        <input name="email" placeholder="email" />
+        <input name="password" placeholder="password" />
+        <input name="passwordconfirmation" placeholder="password confirmation" />
         <button>Sign Up</button>
       </form>
     </div>
   `);
 });
+
+app.post('/', (req, res) => {
+  res.send('Account created!!!')
+});
+
 
 app.listen(3000, () => {
   console.log('Server Running...');
