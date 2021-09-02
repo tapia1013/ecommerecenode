6bf03797f6da208954717e1a3841656aa336cdba
tapia1013 <manbearpig1013@gmail.com>
2021-09-01 18:14:23 -0700

    created own middleware to get email and password info

--- index.js
+++ index.js
@@ -14,7 +14,44 @@ app.get('/', (req, res) => {
   `);
 });
 
-app.post('/', (req, res) => {
+
+
+
+// middleware for app.post
+const bodyParse = (req, res, next) => {
+  if (req.method === 'POST') {
+    req.on('data', data => {
+      // returns a <Buffer 23 23 12 41 /> so we have toString('utf8') to be able to read it 
+      // console.log(data.toString('utf8'));
+
+      // we have to parse it and add info to an object
+      const parsed = data.toString('utf8').split('&');
+      const formData = {};
+
+      // loop through parsed array
+      for (let pair of parsed) {
+        const [key, value] = pair.split('=');
+        formData[key] = value;
+      }
+      // shows this{email: 'asds', passowrd: 'adssad'}
+      // console.log(formData);
+
+
+      req.body = formData;
+      next();
+    });
+  } else {
+    next();
+  }
+}
+
+
+
+
+
+// we put the middleware inbetween '/',HERE,()=>{}
+app.post('/', bodyParse, (req, res) => {
+  console.log(req.body);
   res.send('Account created!!!')
 });
 
