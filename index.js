f03b7cd1ed4dc35d3bd9632f3ff25f9e06f2bd96
tapia1013 <manbearpig1013@gmail.com>
2021-09-01 18:23:16 -0700

    added bodyparser from body-parser that has its own method to parse the form info and we use app.use for us to access it without having to insert it as a callback middleware, app.use bodyParse.urlencoded extended true

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+// bodyparses everywhere in the app so we wont have to keep writing it
+app.use(bodyParse.urlendcoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
   res.send(`
@@ -18,42 +24,46 @@ app.get('/', (req, res) => {
 
 
 // middleware for app.post
-const bodyParse = (req, res, next) => {
-  if (req.method === 'POST') {
-    req.on('data', data => {
-      // returns a <Buffer 23 23 12 41 /> so we have toString('utf8') to be able to read it 
-      // console.log(data.toString('utf8'));
+// const bodyParse = (req, res, next) => {
+//   if (req.method === 'POST') {
+//     req.on('data', data => {
+//       // returns a <Buffer 23 23 12 41 /> so we have toString('utf8') to be able to read it 
+//       // console.log(data.toString('utf8'));
 
-      // we have to parse it and add info to an object
-      const parsed = data.toString('utf8').split('&');
-      const formData = {};
+//       // we have to parse it and add info to an object
+//       const parsed = data.toString('utf8').split('&');
+//       const formData = {};
 
-      // loop through parsed array
-      for (let pair of parsed) {
-        const [key, value] = pair.split('=');
-        formData[key] = value;
-      }
-      // shows this{email: 'asds', passowrd: 'adssad'}
-      // console.log(formData);
+//       // loop through parsed array
+//       for (let pair of parsed) {
+//         const [key, value] = pair.split('=');
+//         formData[key] = value;
+//       }
+//       // shows this{email: 'asds', passowrd: 'adssad'}
+//       // console.log(formData);
 
 
-      req.body = formData;
-      next();
-    });
-  } else {
-    next();
-  }
-}
+//       req.body = formData;
+//       next();
+//     });
+//   } else {
+//     next();
+//   }
+// }
 
 
 
 
 
 // we put the middleware inbetween '/',HERE,()=>{}
-app.post('/', bodyParse, (req, res) => {
+app.post('/', (req, res) => {
   console.log(req.body);
   res.send('Account created!!!')
 });
+
+
+
+
 
 
 app.listen(3000, () => {
