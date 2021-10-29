dfa9d0da919c6458231f0cbffbb48d306f370043
tapia1013 <manbearpig1013@gmail.com>
2021-10-28 19:45:12 -0700

    added new layout and theres a problem with routes to admin product

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const { validationResult } = require('express-validator')
+const productsRepo = require('../../repositories/products');
+const productsNewTemplate = require('../../views/admin/products/new');
+const { requireTitle, requirePrice } = require('./validators');
+
 
 const router = express.Router();
 
@@ -7,9 +12,14 @@ router.get('/admin/products', (req, res) => {
 });
 
 router.get('/admin/products/new', (req, res) => {
-
+  res.send(productsNewTemplate({}));
 });
 
+router.post('/admin/products/new', [requireTitle, requirePrice], () => {
+  const errors = validationResult(req)
+  console.log(errors);
 
+  res.send('submitted');
+})
 
 module.exports = router;
