a8eb052c19cdc9cbb7b2ae2d1571f9c13b93eb5b
tapia1013 <manbearpig1013@gmail.com>
2021-11-03 20:38:20 -0700

    added delete functionality

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -92,7 +92,17 @@ router.post(
 
     res.redirect('/admin/products');
   }
-)
+);
+
+router.post(
+  '/admin/products/:id/delete',
+  requireAuth,
+  async (req, res) => {
+    await productsRepo.delete(req.params.id);
+
+    res.redirect('/admin/products')
+  }
+);
 
 
 module.exports = router;
